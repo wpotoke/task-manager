1ad8e6df980f74eae7a174041b7bc9db6af8e246
moroz <morozovdaniel713@gmail.com>
2025-08-29 14:47:41 +0300

    feat(models): Implement task model with SQLAlchemy
    - Create uuid field for unique task identification
    - Add name and description fields for task details
    - Create task status class with Enum for status choises
    - Add status field with default value "created"
    - Include created_at and updated_at fields for timestamps

---
+++ fastapi_task_manager/app/models/db/task.py
@@ -0,0 +1,47 @@
+import datetime
+import enum
+import uuid
+import sqlalchemy
+from sqlalchemy.orm import (
+    Mapped as SQLAlchemyMapped,
+    mapped_column as sqlalchemy_mapped_column,
+)
+from sqlalchemy.sql import functions as sqlalchemy_functions
+
+from app.repository.table import Base
+
+
+class TaskStatus(enum.Enum):
+    CREATED = "created"
+    WORK = "in_work"
+    DONE = "done"
+
+
+class Task(Base):
+    __tablename__ = "task"
+
+    uuid: SQLAlchemyMapped[uuid.UUID] = sqlalchemy_mapped_column(
+        sqlalchemy.UUID,
+        default=uuid.uuid4,
+        unique=True,
+        nullable=False,
+    )
+    name: SQLAlchemyMapped[str] = sqlalchemy_mapped_column(
+        sqlalchemy.String(length=255), nullable=False
+    )
+    description: SQLAlchemyMapped[str] = sqlalchemy_mapped_column(
+        sqlalchemy.String, nullable=True
+    )
+    status: SQLAlchemyMapped[TaskStatus] = sqlalchemy_mapped_column(
+        sqlalchemy.Enum(TaskStatus), default=TaskStatus.CREATED, nullable=False
+    )
+    created_at: SQLAlchemyMapped[datetime.datetime] = sqlalchemy_mapped_column(
+        sqlalchemy.DateTime(timezone=True),
+        nullable=False,
+        server_default=sqlalchemy_functions.now(),
+    )
+    updated_at: SQLAlchemyMapped[datetime.datetime] = sqlalchemy_mapped_column(
+        sqlalchemy.DateTime(timezone=True),
+        nullable=True,
+        server_onupdate=sqlalchemy.schema.FetchedValue(for_update=True),
+    )
